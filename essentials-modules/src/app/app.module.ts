57c95a41b248262125eb1fe29502037c49ff7199
DanevskiM <danevskimetodija5@gmail.com>
2026-03-10 20:14:56 +0100

    shared-modules

--- essentials-modules/src/app/app.module.ts
+++ essentials-modules/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
-
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports:[BrowserModule, HeaderComponent, UserComponent, TasksComponent]
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
-export class AppModule {
-
-}
+export class AppModule {}
