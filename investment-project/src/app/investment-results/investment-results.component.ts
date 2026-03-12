8208c453d96e8204b64e9551fecf6537b12191ee
DanevskiM <danevskimetodija5@gmail.com>
2026-03-12 14:55:49 +0100

    investment-service

--- investment-project/src/app/investment-results/investment-results.component.ts
+++ investment-project/src/app/investment-results/investment-results.component.ts
@@ -1,19 +1,16 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  @Input() results?: {
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
+  private investmentService = inject(InvestmentService);
+  get results() {
+    return this.investmentService.resultData;
+  }
 }
