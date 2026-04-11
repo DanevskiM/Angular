e37845f3310b461ec0b8c8ef37a825875dfcf6b4
DanevskiM <danevskimetodija5@gmail.com>
2026-04-11 21:13:39 +0200

    canDeactivate_and_reload

--- routing-project/src/app/users/users.routes.ts
+++ routing-project/src/app/users/users.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
-import { NewTaskComponent } from '../tasks/new-task/new-task.component';
+import {
+  canLeaveEditPage,
+  NewTaskComponent,
+} from '../tasks/new-task/new-task.component';
 import { resolveTitle } from './user-tasks/user-tasks.component';
 
 export const routes: Routes = [
@@ -14,7 +17,7 @@ export const routes: Routes = [
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
-    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
     },
@@ -23,5 +26,6 @@ export const routes: Routes = [
   {
     path: 'tasks/new',
     component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage],
   },
 ];
